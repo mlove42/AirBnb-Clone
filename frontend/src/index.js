0ea8938a462c058178de327f914fcd3608c8c683
Michael Love <michael.love42@gmail.com>
2022-10-22 10:31:38 -0700

    phase 3 auth me

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
 import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
@@ -16,13 +19,16 @@ if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.sessionActions = sessionActions;
 }
+
 function Root() {
   return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <Provider store={store}>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
+    </Provider>
   );
 }
 
